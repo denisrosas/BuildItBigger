e0e3459339d5e4562a7a6b73519e88cdd585a4f4
Denis <denis.rosas@gmail.com>
2018-03-08 23:48:02 -0400

    Aumentei a variedade de piadas. Agora sorteia uma piada de uma lista.

--- JokeProviderLib/src/main/java/com/example/android/jokeproviderlib/JokeProvider.java
+++ JokeProviderLib/src/main/java/com/example/android/jokeproviderlib/JokeProvider.java
@@ -1,14 +1,20 @@
 package com.example.android.jokeproviderlib;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class JokeProvider {
 
+    private static ArrayList<String> jokeList = new ArrayList<String>() {{
+        add("O que é, o que é? Feito para andar e não anda?\nR: A Rua.");
+        add("O que é, o que é? Que dá muitas voltas e não sai do lugar?\nR: O relogio.");
+        add("O que é, o que é? Tem cabeça, tem dente, tem barba, não é bicho e nem é gente?\nR: O Alho.");
+        add("O que é, o que é? Que anda com os pés na cabeça?\nR: O Piolho.");
+    }};
+
     public static String getJoke(){
-        return "A professora chega para o Jo&#227;ozinho e diz:\n" +
-                "\n" +
-                " - Jo&#227;ozinho qual &#233; o tempo da frase: Eu procuro um homem fiel?\n" +
-                "\n" +
-                " E ent&#227;o Jo&#227;ozinho responde\n" +
-                "\n" +
-                "- &#201; tempo perdido!\n";
+
+        Random random = new Random();
+        return jokeList.get(random.nextInt(4));
     }
 }
